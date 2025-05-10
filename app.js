d9b02d4e8118aa1b6bca5c4f93b3f697e7612f23
gvasir <simv2022@gmail.com>
2025-05-10 18:29:35 -0400

    all valid and css complete

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const app = express();
 import session from 'express-session';
 import configRoutes from './routes/index.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/static', (req, res, next) => {  
   res.sendStatus(404);
 });
 
 app.use(express.static('public'));
-
 
 app.use(session({
   secret: 'simran wuz here',
